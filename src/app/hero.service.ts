470400f7fcfe10b6fe39865dc456f1b0ade64a00
ajay-sibner <ajay@sibnerpartnership.co.uk>
2023-02-27 07:26:09 +0000

    Final app

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HEROES } from './mock-heroes';
-import {Hero} from './hero';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs';
+
+import { HEROES } from './mock-heroes';
+import {Hero} from './hero';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes'
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private httpClient: HttpClient,
+  ) { }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    this.log('fetched heroes');
+    return this.httpClient.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: Number): Observable<Hero> {
